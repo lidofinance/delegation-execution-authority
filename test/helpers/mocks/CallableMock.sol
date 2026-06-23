335c2b1a7cc526e7af5bc2b6873710ce4566885e
Dmitry Gusakov <gusakov.dv@gmail.com>
2026-06-23 09:28:21 +0200

    chore: Move tests and deploy to foundry

---
+++ test/helpers/mocks/CallableMock.sol
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2026 Lido <info@lido.fi>
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.35;
+
+contract CallableMock {
+    error AlwaysReverts();
+
+    function isOdd(uint256 number) external pure returns (bool) {
+        return number % 2 == 1;
+    }
+
+    function revertsWhenCalled() external pure {
+        revert AlwaysReverts();
+    }
+}
